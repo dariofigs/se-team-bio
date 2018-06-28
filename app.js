4c822e9d103318e7e3800bf5883d149cd7a45523
Kyle Hall <kylehall@MacBook-Air-54.local>
2018-06-28 12:20:54 -0500

    the loop works

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var queryURL = "https://api.myjson.com/bins/16l34m";
+var queryURL = "https://api.myjson.com/bins/17g5ye";
 var jsonTree;
 
 $.ajax({
@@ -6,65 +6,36 @@ $.ajax({
     method: "GET"
 }).done(function (response) {
     var jsonTree = response;
+
     var officeName = []
     var officeClass = []
-    for (var i in jsonTree.offices) {
+
+
+    for (let i in jsonTree.offices) {
         officeName.push(jsonTree.offices[i].name);
         officeClass.push(jsonTree.offices[i].class);
     }
-    var locations = $(".locations");
-    var div = $("<div>");
-    div.addClass("fl w-third mw5 mw7-ns center pa3 ph5-ns chicago fullSizeImage slick-slide " + officeClass[0]);
-    var link = $("<a>");
-    var h2 = $("<h2>").html(officeName[0]);
-    h2.addClass("f2");
-    var span = $("<span>");
-    span.addClass("bar little-bar")
-    link.append(h2);
-    div.append(link);
-    div.append(span);
-    locations.append(div);
 
-    var locations = $(".locations");
-    var div = $("<div>");
-    div.addClass("fl w-third mw5 mw7-ns center pa3 ph5-ns chicago fullSizeImage slick-slide " + officeClass[0]);
-    var link = $("<a>");
-    var h2 = $("<h2>").html(officeName[0]);
-    h2.addClass("f2");
-    var span = $("<span>");
-    span.addClass("bar little-bar")
-    link.append(h2);
-    div.append(link);
-    div.append(span);
-    locations.append(div);
+    for (var i = 0; i < officeName.length; i++) {
+        let locations = $(".locations");
+        let div = $("<div>");
+        div.addClass("fl w-third mw5 mw7-ns center pa3 ph5-ns fullSizeImage slick-slide " + officeClass[i]);
+        let link = $("<a>");
+        let h2 = $("<h2>").html(officeName[i]);
+        h2.addClass("f2");
+        let span = $("<span>");
+        span.addClass("bar little-bar")
+        link.append(h2);
+        div.append(link);
+        div.append(span);
+        locations.append(div);
 
-    var locations = $(".locations");
-    var div = $("<div>");
-    div.addClass("fl w-third mw5 mw7-ns center pa3 ph5-ns chicago fullSizeImage slick-slide " + officeClass[0]);
-    var link = $("<a>");
-    var h2 = $("<h2>").html(officeName[0]);
-    h2.addClass("f2");
-    var span = $("<span>");
-    span.addClass("bar little-bar")
-    link.append(h2);
-    div.append(link);
-    div.append(span);
-    locations.append(div);
+    }
 
-    var locations = $(".locations");
-    var div = $("<div>");
-    div.addClass("fl w-third mw5 mw7-ns center pa3 ph5-ns chicago fullSizeImage slick-slide " + officeClass[0]);
-    var link = $("<a>");
-    var h2 = $("<h2>").html(officeName[0]);
-    h2.addClass("f2");
-    var span = $("<span>");
-    span.addClass("bar little-bar")
-    link.append(h2);
-    div.append(link);
-    div.append(span);
-    locations.append(div);
 
-    runslick(() =>{
+
+
+    runslick(() => {
         $(function () {
             $('.locations').slick({
                 infinite: true,
@@ -73,7 +44,8 @@ $.ajax({
                 cssEase: 'ease-in-out'
             });
         })
-    ;})
+            ;
+    })
 
 });
 
